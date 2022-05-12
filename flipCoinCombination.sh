56802ca977b312d77a112196d044fda81321ecdb
tusharvidwans99 <tusharvidwans99@gmail.com>
2022-05-12 10:59:05 +0530

    Fliping the coin to get Head or Tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,3 +2,12 @@
 echo Welcome to the Flip Coin Combination.
 
 echo "This problem displays winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet."
+
+flipcoin=$((RANDOM%2))
+
+if [ $flipcoin == 1 ]
+then
+	echo Heads win
+else
+	echo Tails win
+fi
